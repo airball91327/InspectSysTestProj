4a021d11d74ce104c184bcce5e99c8822c42a670
JimyHsu <airball91327@gmail.com>
2018-06-06 17:34:42 +0800

    Rebuild the model of "InspectFields", and still working on the model relationship between "InspectItems" and "InspectFields", trying to use foreign key to connect the two tables.

--- InspectSystem/InspectSystem/Models/InspectFields.cs
+++ InspectSystem/InspectSystem/Models/InspectFields.cs
@@ -7,16 +7,23 @@ namespace InspectSystem.Models
     [Table("InspectFields")]
     public class InspectFields
     {
-        [Key]
+        [Key, Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        [Display(Name = "ID")]
+        public int ID { get; set; }         //對應ClassesOfAreas的ID、程式產生
+        [Key, Column(Order = 2)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        [Display(Name = "項目代碼")]
+        public int ItemID { get; set; }
+        [Key, Column(Order = 3)]
         [Required]
         [Display(Name = "欄位代碼")]
         public int FieldID { get; set; }
         [Required]
         [Display(Name = "欄位名稱")]
         public string FieldName { get; set; }
-        [Required]
-        [Display(Name = "項目代碼")]
-        public int ItemID { get; set; }
         [Required]
         [Display(Name = "資料型態")]
         public string DataType { get; set; }
