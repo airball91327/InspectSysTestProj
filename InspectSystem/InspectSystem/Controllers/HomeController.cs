835c347e39911df71f332e9ebd2f731b9115f052
JimyHsu <37923582+airball91327@users.noreply.github.com>
2018-06-13 16:17:14 +0800

    small change

--- InspectSystem/InspectSystem/Controllers/HomeController.cs
+++ InspectSystem/InspectSystem/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace InspectSystem.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return PartialView();
         }
         
     }
