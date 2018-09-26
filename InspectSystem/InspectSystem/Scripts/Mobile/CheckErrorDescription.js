b70753e4de4e1e1fea12b2763f96992c4e99f95c
JimyHsu <37923582+airball91327@users.noreply.github.com>
2018-09-26 21:18:30 +0800

    Set login detail for user from userID to userName, and add a confirm msg before user start to inspect, and working on checking when radio button set false, the error msg should be required.

---
+++ InspectSystem/InspectSystem/Scripts/Mobile/CheckErrorDescription.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    $("input:radio").change(function () {
+        var objName = this.name.toString();
+        var cutName = objName.split(".", 1);
+        var targetId = cutName + ".ErrorDescription"
+        var value = this.nodeValue;
+        document.getElementById(targetId).setAttribute("required", "required");
+        alert(objName + ":" + cutName + ":" + targetId + ":" + value);
+    });
+
+});
