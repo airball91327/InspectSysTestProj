3dedc0f4185efd32a34473a5e1d47fd15c55ef23
JimyHsu <airball91327@gmail.com>
2018-10-08 17:26:02 +0800

    Import jquery.datatables plug-in, and working on search system.

---
+++ InspectSystem/InspectSystem/Controllers/InspectDocSearchController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InspectSystem.Models;
+
+
+namespace InspectSystem.Controllers
+{
+    public class InspectDocSearchController : Controller
+    {
+
+        private BMEDcontext db = new BMEDcontext();
+
+        // GET: InspectDocSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: InspectDocSearch/GetData
+        public JsonResult GetData()
+        {
+            int FlowStatus = 2;
+            try
+            {
+                var resultList = ((from i in db.InspectDocs
+                                   where i.FlowStatusID == FlowStatus
+                                   select new
+                                   {
+                                       AreaName = i.InspectAreas.AreaName,
+                                       FlowStatusName = i.FlowStatusID,
+                                       Date = i.Date,
+                                       WorkerID = i.WorkerID,
+                                       WorkerName = i.WorkerName,
+                                       CheckerID = i.CheckerID,
+                                       CheckerName = i.CheckerName
+                                   }).ToList());
+                return Json(resultList, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
