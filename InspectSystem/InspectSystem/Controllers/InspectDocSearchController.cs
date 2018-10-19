486823c2e808da2912fdc63420798bb4e76c6689
JimyHsu <airball91327@gmail.com>
2018-10-19 15:33:58 +0800

    1. Search request is working fine to send to controller. 2. Search result paging is completed. 3. Start to implement search result in controller. 4. Start to implement fliter, and hear filter.

--- InspectSystem/InspectSystem/Controllers/InspectDocSearchController.cs
+++ InspectSystem/InspectSystem/Controllers/InspectDocSearchController.cs
@@ -20,12 +20,18 @@ namespace InspectSystem.Controllers
         // GET: InspectDocSearch
         public ActionResult Index()
         {
+            ViewBag.AreaID = new SelectList(db.InspectAreas, "AreaID", "AreaName");
+            ViewBag.FlowStatusID = new SelectList(db.InspectFlowStatusTable, "FlowStatusID", "FlowStatusName");
             return View();
         }
 
-        // GET: InspectDocSearch/GetData
-        public JsonResult GetData()
+        // GET: InspectDocSearch/GetData          
+        public JsonResult GetData(int? draw, int? start, int length,    //←此三個為DataTables自動傳遞參數
+                                  string startDate, string endDate, int? areaId, int? flowStatusId)     //←為表單的查詢條件
         {
+            //查詢&排序後的總筆數
+            int recordsTotal = 0;
+
             int FlowStatus = 2;
             try
             {
@@ -34,14 +40,26 @@ namespace InspectSystem.Controllers
                                    select new
                                    {
                                        AreaName = i.InspectAreas.AreaName,
-                                       FlowStatusName = i.FlowStatusID,
+                                       FlowStatusName = i.InspectFlowStatusTable.FlowStatusName,
                                        Date = i.Date,
                                        WorkerID = i.WorkerID,
                                        WorkerName = i.WorkerName,
                                        CheckerID = i.CheckerID,
                                        CheckerName = i.CheckerName
                                    }).ToList());
-                return Json(resultList, JsonRequestBehavior.AllowGet);
+
+                recordsTotal = resultList.Count();//查詢後的總筆數
+
+                //回傳Json資料
+                var returnObj =
+                    new
+                    {
+                      draw = draw,
+                      recordsTotal = recordsTotal,
+                      recordsFiltered = recordsTotal,
+                      data = resultList.Skip(start ?? 0).Take(length)   //分頁後的資料 
+                    };
+                return Json(returnObj, JsonRequestBehavior.AllowGet);
             }
             catch (Exception)
             {
