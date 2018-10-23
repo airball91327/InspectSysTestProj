8d2bfd2017b2fbd23a6ac5134662f24558736c90
JimyHsu <airball91327@gmail.com>
2018-10-23 17:34:00 +0800

    1. Add system.linq.dynamic libary. 2. Complete to view doc details in search system. 3. Order by columns is completed.

--- InspectSystem/InspectSystem/Controllers/InspectDocSearchController.cs
+++ InspectSystem/InspectSystem/Controllers/InspectDocSearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Dynamic;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
@@ -32,6 +33,12 @@ namespace InspectSystem.Controllers
         {
             //查詢&排序後的總筆數
             int recordsTotal = 0;
+            //jQuery DataTable的Column index
+            string col_index = Request.QueryString["order[0][column]"];
+            //排序資料行名稱
+            string sortColName = string.IsNullOrEmpty(col_index) ? "Date" : Request.QueryString[$@"columns[{col_index}][data]"];
+            //升冪或降冪
+            string asc_desc = string.IsNullOrEmpty(Request.QueryString["order[0][dir]"]) ? "asc" : Request.QueryString["order[0][dir]"];//防呆
 
             try
             {
@@ -74,7 +81,18 @@ namespace InspectSystem.Controllers
                     DocID = s.DocID,
                     AreaID = s.AreaID,
                     FlowStatusID = s.FlowStatusID
-                }).ToList().OrderBy(r => r.Date);
+                }).ToList();
+
+                // Deal DataTable sorting. 
+                if(sortColName == "AreaName")
+                {
+                    sortColName = "AreaID";
+                }
+                else if(sortColName == "FlowStatusName")
+                {
+                    sortColName = "FlowStatusID";
+                }
+                resultList = resultList.AsEnumerable().OrderBy($@"{sortColName} {asc_desc}").ToList();
 
                 recordsTotal = resultList.Count();//查詢後的總筆數
 
@@ -92,6 +110,78 @@ namespace InspectSystem.Controllers
             catch (Exception)
             {
                 throw;
+            }
+        }
+
+        // GET: InspectDocSearch/DocDetailsIndex
+        public ActionResult DocDetailsIndex(int docID)
+        {
+            /* Set variables from DB. */
+            var DocDetailList = db.InspectDocDetails.Where(i => i.DocID == docID).ToList();
+            int length = docID.ToString().Length;
+            int areaID = System.Convert.ToInt32(docID.ToString().Substring(length - 2));
+
+            ViewBag.AreaID = DocDetailList.First().AreaID;
+            ViewBag.AreaName = DocDetailList.First().AreaName;
+            ViewBag.DocID = docID;
+
+            /* Find Classes from DocDetails and set values to List<ClassesOfAreas> ClassList. */
+            var ClassesOfDocTemp = DocDetailList.GroupBy(c => c.ClassID).Select(g => g.FirstOrDefault()).ToList();
+            List<ClassesOfAreas> ClassList = new List<ClassesOfAreas>();
+            foreach (var itemClass in ClassesOfDocTemp)
+            {
+                var addClass = db.ClassesOfAreas.Where(c => c.AreaID == areaID && c.ClassID == itemClass.ClassID).FirstOrDefault();
+                ClassList.Add(addClass);
+            }
+            var ClassesOfAreas = ClassList.OrderBy(c => c.InspectClasses.ClassOrder);
+
+            /* Count errors for every class, and set count result to "CountErrors". */
+            foreach (var item in ClassesOfAreas)
+            {
+                var toFindErrors = DocDetailList.Where(d => d.ClassID == item.ClassID &&
+                                                           d.IsFunctional == "n");
+                item.CountErrors = toFindErrors.Count();
+            }
+            return View(ClassesOfAreas.ToList());
+        }
+
+        // GET: InspectDocSearch/DocTempIndex
+        public ActionResult DocTempIndex(int docID)
+        {
+            var DocDetailList = db.InspectDocDetailsTemporary.Where(i => i.DocID == docID).ToList();
+            int length = docID.ToString().Length;
+            int areaID = System.Convert.ToInt32(docID.ToString().Substring(length - 2));
+
+            if (DocDetailList.Count == 0)
+            {
+                TempData["SaveMsg"] = "尚未有資料儲存";
+                return RedirectToAction("Index", new { AreaID = areaID });
+            }
+            else
+            {
+                ViewBag.AreaID = DocDetailList.First().AreaID;
+                ViewBag.AreaName = DocDetailList.First().AreaName;
+                ViewBag.DocID = docID;
+
+                /* Find Classes from DocDetails and set values to List<ClassesOfAreas> ClassList. */
+                var ClassesOfDocTemp = DocDetailList.GroupBy(c => c.ClassID).Select(g => g.FirstOrDefault()).ToList();
+                List<ClassesOfAreas> ClassList = new List<ClassesOfAreas>();
+                foreach (var itemClass in ClassesOfDocTemp)
+                {
+                    var addClass = db.ClassesOfAreas.Where(c => c.AreaID == areaID && c.ClassID == itemClass.ClassID).FirstOrDefault();
+                    ClassList.Add(addClass);
+                }
+                var ClassesOfAreas = ClassList.OrderBy(c => c.InspectClasses.ClassOrder);
+
+                /* Count errors for every class, and set count result to "CountErrors". */
+                foreach (var item in ClassesOfAreas)
+                {
+                    var toFindErrors = DocDetailList.Where(d => d.ClassID == item.ClassID &&
+                                                               d.IsFunctional == "n");
+                    item.CountErrors = toFindErrors.Count();
+                }
+
+                return View(ClassesOfAreas.ToList());
             }
         }
 
