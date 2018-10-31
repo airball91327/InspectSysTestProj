061612747b80a7099199df5ff605e85cf43b8c65
JimyHsu <airball91327@gmail.com>
2018-10-31 12:17:29 +0800

    1. Start to design report views.

---
+++ InspectSystem/InspectSystem/Controllers/InspectReportsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InspectSystem.Controllers
+{
+    public class InspectReportsController : Controller
+    {
+        // GET: InspectReports
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
