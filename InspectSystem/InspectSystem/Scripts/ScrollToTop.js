90c4107303fbcb612f0b9950f86d79b2c4cce74d
JimyHsu <airball91327@gmail.com>
2019-08-29 14:13:49 +0800

    1. Add sticky position button for edit views on mobile.

--- InspectSystem/InspectSystem/Scripts/ScrollToTop.js
+++ InspectSystem/InspectSystem/Scripts/ScrollToTop.js
@@ -9,9 +9,15 @@
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("goToTopBtn").style.display = "block";
+        document.getElementById("submitScrollBtn").style.display = "block";
     } else {
         document.getElementById("goToTopBtn").style.display = "none";
+        document.getElementById("submitScrollBtn").style.display = "none";
     }
+    /* If scroll to bottom. */
+    if (document.documentElement.scrollTop + window.innerHeight == document.documentElement.scrollHeight) {
+        document.getElementById("submitScrollBtn").style.display = "none";
+    } 
 }
 
 // When the user clicks on the button, scroll to the top of the document
